766c0d06c692009541db03eca0bce718342c2f05
Carl Peterken <carboes@gmail.com>
2016-10-26 12:01:31 +1300

    updated to retrieve function rather than dispatch object

--- src/index.js
+++ src/index.js
@@ -3,64 +3,64 @@ import Promise from 'promise';
 
 const asyncPoll = (intervalDuration = 60 * 1000, onInterval) => {
 
-	return (Component) => class extends React.Component {
-		constructor () {
-			super();
-			this.startPolling = this.startPolling.bind(this);
-			this.stopPolling = this.stopPolling.bind(this);
-		}
+  return (Component) => class extends React.Component {
+    constructor () {
+      super();
+      this.startPolling = this.startPolling.bind(this);
+      this.stopPolling = this.stopPolling.bind(this);
+    }
 
-		componentDidMount () {
-			this.startPolling();
-		}
+    componentDidMount () {
+      this.startPolling();
+    }
 
-		componentWillUnmount () {
-			this.stopPolling();
-		}
+    componentWillUnmount () {
+      this.stopPolling();
+    }
 
-		startPolling () {
-			if (this.interval) return;
-			this.keepPolling = true;
-			this.asyncInterval(intervalDuration, onInterval);
-		}
+    startPolling () {
+      if (this.interval) return;
+      this.keepPolling = true;
+      this.asyncInterval(intervalDuration, onInterval);
+    }
 
-		stopPolling () {
-			this.keepPolling = false;
-			if (this.interval) clearTimeout(this.interval);
-		}
+    stopPolling () {
+      this.keepPolling = false;
+      if (this.interval) clearTimeout(this.interval);
+    }
 
-		asyncInterval (intervalDuration, fn) {
-			const promise = fn(this.getProps(), this.props.dispatch);
-			const asyncTimeout = () => setTimeout(() => {
-				this.asyncInterval(intervalDuration, fn);
-			}, intervalDuration);
-			const assignNextInterval = () => {
-				if (!this.keepPolling) {
-					this.stopPolling();
-					return;
-				}
-				this.interval = asyncTimeout();
-			};
+    asyncInterval (intervalDuration, fn) {
+      const promise = fn(this.getProps(), this.props.onPollInterval);
+      const asyncTimeout = () => setTimeout(() => {
+        this.asyncInterval(intervalDuration, fn);
+      }, intervalDuration);
+      const assignNextInterval = () => {
+        if (!this.keepPolling) {
+          this.stopPolling();
+          return;
+        }
+        this.interval = asyncTimeout();
+      };
 
-			Promise.resolve(promise)
-			.then(assignNextInterval)
-			.catch(assignNextInterval);
-		}
+      Promise.resolve(promise)
+      .then(assignNextInterval)
+      .catch(assignNextInterval);
+    }
 
-		getProps () {
-			return {
-				...this.props,
-				startPolling: this.startPolling,
-				stopPolling: this.stopPolling,
-				isPolling: Boolean(this.interval),
-			};
-		}
+    getProps () {
+      return {
+        ...this.props,
+        startPolling: this.startPolling,
+        stopPolling: this.stopPolling,
+        isPolling: Boolean(this.interval),
+      };
+    }
 
-		render () {
-			const props = this.getProps();
-			return <Component {...props} />;
-		}
-	};
+    render () {
+      const props = this.getProps();
+      return <Component {...props} />;
+    }
+  };
 
 };
 
